b41af8f5c61e8d1f066f2e0dc1bffd23a05f2187
JohnKHW <wongkahingerv@gmail.com>
2024-03-11 12:46:05 +0800

    style: update getDomainType in new format

--- packages/core/src/utils/getDomainType.ts
+++ packages/core/src/utils/getDomainType.ts
@@ -1,6 +1,28 @@
 import tlds from "../data/tlds.json";
-import { ITldInfo, ITldList } from "../interfaces";
-export const getDomainType = (fqdn: string) => {
+
+export const getDomainType = (
+  fqdn: string
+): "classical" | "universal" | "omni" => {
   const [tld] = fqdn.split(".").reverse();
-  return (tlds as ITldList)[tld].type;
+
+  for (const chain of tlds.classical) {
+    if (chain.names.includes(tld)) {
+      return "classical";
+    }
+  }
+
+  for (const category of tlds.universal) {
+    if (category.names.includes(tld)) {
+      return "universal";
+    }
+  }
+
+  for (const category of tlds.omni) {
+    if (category.names.includes(tld)) {
+      return "omni";
+    }
+  }
+  throw new Error("Unknown domain type");
+  // const isUniversal;
+  // return (tlds as ITldList)[tld].type;
 };
